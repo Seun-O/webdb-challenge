13becfdb193c1ffee223f8ed5aa183b8299fd100
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-06-02 22:34:58 -0400

    DB Handles complete

--- server/database/dbHelpers.js
+++ server/database/dbHelpers.js
@@ -10,6 +10,12 @@ const config = {
 
 const db = knex(config);
 
+/*
+    getProject takes optional parameter ID which would return a project based on that ID.
+    !ID get project returns all projects in the database
+    SQL SELECT * FROM projects || SELECT * FROM projects WHERE projects.id = ID
+*/
+
 const getProject = id => {
   if (id) {
     return db("projects as p")
@@ -19,6 +25,12 @@ const getProject = id => {
   return db("projects");
 };
 
+/*
+    getAction takes optional parameter ID which would return the action related to the ID.
+    !ID getAction returns all actions in the database
+    SQL SELECT * FROM actions || SELECT * FROM actions WHERE actions.id = ID
+
+*/
 const getAction = id => {
   if (id) {
     return db("actions")
@@ -28,22 +40,30 @@ const getAction = id => {
   return db("actions");
 };
 
+/*
+    addAction inserts a new action into the database.
+    required fields: name, description, completed, project
+    optional fields: notes
+    SQL INSERT INTO actions (name, description, notes, completed, project)
+    VALUES ("name", "description", "notes", "true/false", "project id")
+*/
 const addAction = action => {
   return db("actions").insert(action);
 };
+
+/*
+    addProject inserts a new project into the database.
+    required fields: name, description, completed
+    SQL INSERT INTO projects (name, description, completed)
+    VALUES ("name", "description", "true/false",)
+*/
 const addProject = project => {
   return db("projects").insert(project);
 };
 
+//Function to test db helper methods without endpoints
 async function execute() {
   try {
-    // const data = await addAction({
-    //   name: "Test action 5",
-    //   description: "Test Action",
-    //   notes: "Test Notes",
-    //   completed: false,
-    //   project: 3
-    // });
     const data = await getAction();
     console.log(data);
   } catch (err) {
